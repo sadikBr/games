55a3399377eabe577628881cbc925993752b31be
SADIK Brahim <62760511+sadikBr@users.noreply.github.com>
2023-04-27 21:29:50 +0100

    adding three-js folder

---
+++ three-js/base-project/src/main.ts
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+class World {
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+  objects: THREE.Mesh[];
+
+  constructor() {
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      innerWidth / innerHeight,
+      0.1,
+      1000
+    );
+    this.camera.position.z = 5;
+    this.scene.add(this.camera);
+
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: document.getElementById('canvas') as HTMLCanvasElement,
+    });
+    this.renderer.setSize(innerWidth, innerHeight);
+    this.objects = [];
+
+    new OrbitControls(this.camera, this.renderer.domElement);
+
+    const ambientLight = new THREE.AmbientLight();
+    this.scene.add(ambientLight);
+
+    const light = new THREE.SpotLight();
+    light.position.set(-5, 5, 5);
+    light.lookAt(new THREE.Vector3(0, 0, 0));
+    this.scene.add(light);
+
+    // Start Animating the world
+    this.createBox();
+    this.animate();
+  }
+
+  createBox() {
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshStandardMaterial({ color: 'green' })
+    );
+    this.objects.push(box);
+    this.scene.add(box);
+  }
+
+  animate() {
+    requestAnimationFrame(() => this.animate());
+
+    this.objects.forEach((object) => {
+      object.rotation.x += 0.02;
+      object.rotation.y += 0.02;
+      object.rotation.z += 0.02;
+    });
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  resize() {
+    this.camera.aspect = innerWidth / innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(innerWidth, innerHeight);
+  }
+}
+
+const world = new World();
+
+addEventListener('resize', () => {
+  world.resize();
+});
