fcc35f6cc137499ce5490fad444fccb1547307ff
SADIK Brahim <62760511+sadikBr@users.noreply.github.com>
2023-04-27 21:33:54 +0100

    changes in the base object

--- three-js/base-project/src/main.ts
+++ three-js/base-project/src/main.ts
@@ -5,7 +5,7 @@ class World {
   scene: THREE.Scene;
   camera: THREE.PerspectiveCamera;
   renderer: THREE.WebGLRenderer;
-  objects: THREE.Mesh[];
+  objects: Map<string, THREE.Mesh>;
 
   constructor() {
     this.scene = new THREE.Scene();
@@ -24,7 +24,7 @@ class World {
       canvas: document.getElementById('canvas') as HTMLCanvasElement,
     });
     this.renderer.setSize(innerWidth, innerHeight);
-    this.objects = [];
+    this.objects = new Map();
 
     new OrbitControls(this.camera, this.renderer.domElement);
 
@@ -37,27 +37,23 @@ class World {
     this.scene.add(light);
 
     // Start Animating the world
-    this.createBox();
     this.animate();
   }
 
-  createBox() {
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial({ color: 'green' })
-    );
-    this.objects.push(box);
-    this.scene.add(box);
+  addObject(name: string, object: THREE.Mesh) {
+    this.objects.set(name, object);
+    this.scene.add(object);
   }
 
   animate() {
     requestAnimationFrame(() => this.animate());
 
-    this.objects.forEach((object) => {
-      object.rotation.x += 0.02;
-      object.rotation.y += 0.02;
-      object.rotation.z += 0.02;
-    });
+    const box = this.objects.get('box');
+    if (box != undefined) {
+      box.rotation.x += 0.02;
+      box.rotation.y += 0.02;
+      box.rotation.z += 0.02;
+    }
 
     this.renderer.render(this.scene, this.camera);
   }
@@ -71,6 +67,12 @@ class World {
 }
 
 const world = new World();
+
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshStandardMaterial({ color: 'green' })
+);
+world.addObject('box', box);
 
 addEventListener('resize', () => {
   world.resize();
