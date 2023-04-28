472bb53577793883f16b99d1640dd8565a15a796
SADIK Brahim <62760511+sadikBr@users.noreply.github.com>
2023-04-28 17:13:33 +0100

    finished three.js hover effect

--- three-js/three-js-hover-effect/src/main.ts
+++ three-js/three-js-hover-effect/src/main.ts
@@ -44,6 +44,7 @@ class World {
   renderer: THREE.WebGLRenderer;
   objects: Map<string, THREE.Mesh>;
   interface: DatGUI;
+  frameCount: number;
 
   constructor() {
     this.scene = new THREE.Scene();
@@ -54,7 +55,7 @@ class World {
       0.1,
       1000
     );
-    this.camera.position.z = 5;
+    this.camera.position.z = 50;
     this.scene.add(this.camera);
 
     this.renderer = new THREE.WebGLRenderer({
@@ -65,15 +66,17 @@ class World {
     this.objects = new Map();
 
     this.interface = new DatGUI();
+    this.frameCount = 0;
 
     new OrbitControls(this.camera, this.renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-    this.scene.add(ambientLight);
-
     const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(0, 0, 5);
+    light.position.set(0, 1, 1);
     this.scene.add(light);
+
+    const backLight = new THREE.DirectionalLight(0xffffff, 1);
+    backLight.position.set(0, 0, -1);
+    this.scene.add(backLight);
 
     // Start Animating the world
     this.animate();
@@ -86,6 +89,7 @@ class World {
 
   animate() {
     requestAnimationFrame(() => this.animate());
+    this.frameCount += 0.01;
 
     const plane = this.objects.get('plane');
     if (plane != undefined) {
@@ -94,13 +98,26 @@ class World {
         this.camera
       );
 
+      // @ts-ignore
+      const { array, originalPosition, randomValues } =
+        plane.geometry.attributes.position;
+      for (let i = 0; i < array.length; i += 3) {
+        array[i] =
+          originalPosition[i] +
+          Math.cos(this.frameCount + randomValues[i]) * 0.006;
+        array[i + 1] =
+          originalPosition[i + 1] +
+          Math.sin(this.frameCount + randomValues[i + 1]) * 0.006;
+      }
+      plane.geometry.attributes.position.needsUpdate = true;
+
       const intersects = this.raycaster.intersectObject(plane);
 
       if (intersects.length > 0) {
         const initialColor = {
-          r: 0,
-          g: 0.025,
-          b: 0.2,
+          r: 0.01,
+          g: 0.03,
+          b: 0.12,
         };
 
         const hoverColor = {
@@ -186,7 +203,7 @@ class World {
 const world = new World();
 
 const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(35, 35, 40, 40),
+  new THREE.PlaneGeometry(500, 500, 65, 65),
   new THREE.MeshPhongMaterial({
     // color: 'green',
     side: THREE.DoubleSide,
@@ -198,21 +215,33 @@ const plane = new THREE.Mesh(
 function customizePlane(plane: THREE.Mesh) {
   // @ts-ignore
   const { array } = plane.geometry.attributes.position;
-  for (let i = 0; i < array.length; i += 3) {
-    const x = array[i];
-    const y = array[i + 1];
-    const z = array[i + 2];
+  const randomValues = [];
 
-    array[i] = x + Math.random() - 0.5;
-    array[i + 1] = y + Math.random() - 0.5;
-    array[i + 2] = z + Math.random() - 0.5;
+  for (let i = 0; i < array.length; i += 1) {
+    if (i % 3 === 0) {
+      const x = array[i];
+      const y = array[i + 1];
+      const z = array[i + 2];
 
-    // TODO: Do something here
+      array[i] = x + (Math.random() - 0.5) * 2;
+      array[i + 1] = y + (Math.random() - 0.5) * 2;
+      array[i + 2] = z + (Math.random() - 0.5) * 10;
+    }
+
+    randomValues.push((Math.random() - 0.5) * 15);
   }
+
+  // @ts-ignore
+  plane.geometry.attributes.position.randomValues = randomValues;
+
+  // @ts-ignore
+  plane.geometry.attributes.position.originalPosition =
+    // @ts-ignore
+    plane.geometry.attributes.position.array;
 
   const colors: number[] = [];
   for (let i = 0; i < plane.geometry.attributes.position.count; i++) {
-    colors.push(0, 0.025, 0.2);
+    colors.push(0.01, 0.03, 0.12);
   }
   // @ts-ignore
   plane.geometry.setAttribute(
@@ -238,11 +267,17 @@ addEventListener('mousemove', (event) => {
 // Dat.GUI interface
 const worldObject = {
   plane: {
-    width: 35,
-    height: 35,
-    segments: 40,
+    width: 500,
+    height: 500,
+    segments: 65,
   },
 };
-world.interface.addPlaneProperty(worldObject.plane, 'width', 10, 50, plane);
-world.interface.addPlaneProperty(worldObject.plane, 'height', 10, 50, plane);
-world.interface.addPlaneProperty(worldObject.plane, 'segments', 10, 50, plane);
+world.interface.addPlaneProperty(worldObject.plane, 'width', 10, 1000, plane);
+world.interface.addPlaneProperty(worldObject.plane, 'height', 10, 1000, plane);
+world.interface.addPlaneProperty(
+  worldObject.plane,
+  'segments',
+  10,
+  1000,
+  plane
+);
