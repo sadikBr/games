5981ca57e98a28fa48130dd08be71dba5e1c2d75
SADIK Brahim <sadbrah@gmail.com>
2025-09-18 11:27:26 +0100

    changes

--- godot/testing-projects/hand-of-cards/game.gd
+++ godot/testing-projects/hand-of-cards/game.gd
@@ -3,11 +3,11 @@ extends Node2D
 @onready var cursor_raycast: RayCast2D = $CursorRaycast
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	cursor_raycast.global_position = get_global_mouse_position()
-	
+
 	var collision = cursor_raycast.is_colliding()
-	
+
 	if collision:
 		var collider = cursor_raycast.get_collider()
 		var collider_parent = collider.get_parent()
--- godot/testing-projects/sierpinski-triangle/sierpinski-triangle.gd
+++ godot/testing-projects/sierpinski-triangle/sierpinski-triangle.gd
@@ -2,31 +2,35 @@ extends ColorRect
 
 @export var max_depth := 5  # Adjust this in the inspector!
 @export var start_length := 600.0   # Initial triangle size
+@export var something := Color.GREEN
+
+func _process(delta: float) -> void:
+  print(delta)
 
 func _draw() -> void:
-	var start_point := Vector2(get_viewport_rect().size.x/2, 0)  # Center-top
-	draw_sierpinski(start_point, start_length, max_depth)
+  var start_point := Vector2(get_viewport_rect().size.x/2, 0)  # Center-top
+  draw_sierpinski(start_point, start_length, max_depth)
 
-func draw_sierpinski(top: Vector2, size: float, depth: int) -> void:
-	if depth <= 0:
-		# Base case: Draw actual triangle
-		var height := (sqrt(3)/2.0) * size
-		var left := Vector2(top.x - size/2, top.y + height)
-		var right := Vector2(top.x + size/2, top.y + height)
-		draw_primitive(PackedVector2Array([top, left, right]), [Color.GREEN_YELLOW], [])
-		return
-	
-	# Calculate positions for smaller triangles
-	var half_size := size/2
-	var small_height := (sqrt(3)/2.0) * half_size
-	
-	# 1. Top triangle (same position)
-	draw_sierpinski(top, half_size, depth - 1)
-	
-	# 2. Left triangle
-	var left_pos := Vector2(top.x - size/4, top.y + small_height)
-	draw_sierpinski(left_pos, half_size, depth - 1)
-	
-	# 3. Right triangle
-	var right_pos := Vector2(top.x + size/4, top.y + small_height)
-	draw_sierpinski(right_pos, half_size, depth - 1)
+func draw_sierpinski(top: Vector2, triangle_size: float, depth: int) -> void:
+  if depth <= 0:
+  # Base case: Draw actual triangle
+    var height := (sqrt(3)/2.0) * triangle_size
+    var left := Vector2(top.x - triangle_size/2, top.y + height)
+    var right := Vector2(top.x + triangle_size/2, top.y + height)
+    draw_primitive(PackedVector2Array([top, left, right]), [Color.GREEN_YELLOW], [])
+    return
+
+  # Calculate positions for smaller triangles
+  var half_size := triangle_size/2
+  var small_height := (sqrt(3)/2.0) * half_size
+
+  # 1. Top triangle (same position)
+  draw_sierpinski(top, half_size, depth - 1)
+
+  # 2. Left triangle
+  var left_pos := Vector2(top.x - triangle_size/4, top.y + small_height)
+  draw_sierpinski(left_pos, half_size, depth - 1)
+
+  # 3. Right triangle
+  var right_pos := Vector2(top.x + triangle_size/4, top.y + small_height)
+  draw_sierpinski(right_pos, half_size, depth - 1)
